3e1d465eb5ab2e2c0671fbaff9cf66598ef87226
Dan <daniel.lenehan.work@gmail.com>
2023-09-17 11:05:05 +0100

    /Users/dan/virtualenvs/kuda/bin/python

--- data_engineering/scripts/workout_parse_test_runs.py
+++ data_engineering/scripts/workout_parse_test_runs.py
@@ -1,16 +1,18 @@
 import json
+
 import requests
+
 from kuda.scrapers.workout.parser import parse_workout_html
 
-
 broken = [
+    "https://bodyspace.bodybuilding.com/workouts/viewworkoutlog/azmatalipasha/56e6ef2c0cf250667f39a72e"
 ]
 
-html = requests.get(broken[0]).text
+html = requests.get(broken[0], timeout=20).text
 
 workout = parse_workout_html(
-	html_text=html,
-	url=broken[0],
+    html_text=html,
+    url=broken[0],
 )
 
 with open("data/workout_links/parse_test.json", "w", encoding="utf-8") as f:
