6a43a5d2bfdf086029ac3202757a90d335dd7d4c
Dan <daniel.lenehan.work@gmail.com>
2023-09-20 09:43:29 +0100

    Test run file update

--- data_engineering/scripts/workout_parse_test_runs.py
+++ data_engineering/scripts/workout_parse_test_runs.py
@@ -4,15 +4,17 @@ import requests
 
 from kuda.scrapers.workout.parser import parse_workout_html
 
-broken = [
-    "https://bodyspace.bodybuilding.com/workouts/viewworkoutlog/azmatalipasha/56e6ef2c0cf250667f39a72e"
-]
+broken = {
+	"body": ["https://bodyspace.bodybuilding.com/workouts/viewworkoutlog/52vizcaino/568065660cf215707caff8cb"],
+	"bucket_key": "workout-data/male/age_21_25"
+}
 
-html = requests.get(broken[0], timeout=20).text
+
+html = requests.get(broken["body"][0], timeout=20).text
 
 workout = parse_workout_html(
     html_text=html,
-    url=broken[0],
+    url=broken["body"][0],
 )
 
 with open("data/workout_links/parse_test.json", "w", encoding="utf-8") as f:
